238bfe05a40106c1328b6721e2e5045fc8ac621f
Zhenxi33 <hemsocheat82@gmail.com>
2025-10-21 22:44:15 +0700

    Check it

--- SLLhead.hpp
+++ SLLhead.hpp
@@ -27,6 +27,83 @@ class SLLh{
             head = nullptr;
             counter = 0;
         }
+        //method
+         // Insert at front
+        void push_front(int val){
+            Node *newNode = new Node(val);
+            newNode->next = head;
+            head = newNode;
+            counter++;
+        }
+        
+        // Insert at back
+        void push_back(int val){
+            Node *newNode = new Node(val);
+            
+            if (head == nullptr) {
+                head = newNode;
+            } else {
+                Node* current = head;
+                while (current->next != nullptr) {
+                    current = current->next;
+                }
+                current->next = newNode;
+            }
+            counter++;
+        }
+        
+        // Delete from front
+        void pop_front(){
+            if (head == nullptr) {
+                cout << "List is empty! Cannot pop_front." << endl;
+                return;
+            }
+            
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            counter--;
+        }
+        
+        // Delete from back
+        void pop_back(){
+            if (head == nullptr) {
+                cout << "List is empty! Cannot pop_back." << endl;
+                return;
+            }
+            
+            if (head->next == nullptr) {
+                delete head;
+                head = nullptr;
+            } else {
+                Node* current = head;
+                while (current->next->next != nullptr) {
+                    current = current->next;
+                }
+                delete current->next;
+                current->next = nullptr;
+            }
+            counter--;
+        }
+        
+        // Display the list
+        void display() {
+            if (head == nullptr) {
+                cout << "List is empty" << endl;
+                return;
+            }
+            
+            Node* current = head;
+            while (current != nullptr) {
+                cout << current->data;
+                if (current->next != nullptr) {
+                    cout << " -> ";
+                }
+                current = current->next;
+            }
+            cout << " -> nullptr" << endl;
+        }
+
         // Destructor
         ~SLLh() {
             while (head != nullptr) {
